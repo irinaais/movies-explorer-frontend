ff28c5cddb060eead091bb7e74c333b9b704d01f
Iris <irinaais90@yandex.ru>
2022-09-11 21:04:17 +0300

    feat: 1) update README.md 2) add optimized images 3) test App.js

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import avatar from '../../images/avatar.png';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <img alt = 'аватар' src={avatar}/>
+    </div>
+  );
+}
+
+export default App;
