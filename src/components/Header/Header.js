e6f604da997852c5fc445e6080f068b89796592a
Iris <irinaais90@yandex.ru>
2022-09-12 21:07:35 +0300

    feat: create Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+import logo from "../../images/logo.svg";
 
 function Header() {
   return (
-    <header>
-
+    <header className="header header_theme_light">
+      <div className="header__container">
+        <img alt="Логотип" src={ logo } className="header__logo"/>
+        <div className="header__button_container">
+          <a className="header__button header__button_signup" href="/signup">Регистрация</a>
+          <a className="header__button header__button_signin" href="/signin">Войти</a>
+        </div>
+      </div>
     </header>
   );
 }
