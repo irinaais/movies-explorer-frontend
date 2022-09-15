f6b232f88d738e42db9e0402b301ef05798fbd80
Iris <irinaais90@yandex.ru>
2022-09-15 14:58:43 +0300

    feat: create Techs

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -8,6 +8,29 @@ function Techs() {
         <h2 className="techs__header">Технологии</h2>
         <h3 className="techs__title">7 технологий</h3>
         <p className="techs__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+        <ul className="techs__list">
+          <li className="techs__item">
+            <p className="techs__item-text">HTML</p>
+          </li>
+          <li className="techs__item">
+            <p className="techs__item-text">CSS</p>
+          </li>
+          <li className="techs__item">
+            <p className="techs__item-text">JS</p>
+          </li>
+          <li className="techs__item">
+            <p className="techs__item-text">React</p>
+          </li>
+          <li className="techs__item">
+            <p className="techs__item-text">Git</p>
+          </li>
+          <li className="techs__item">
+            <p className="techs__item-text">Express.js</p>
+          </li>
+          <li className="techs__item">
+            <p className="techs__item-text">mongoDB</p>
+          </li>
+        </ul>
       </div>
     </section>
   );
