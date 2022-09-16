a91f4dbbdf837012ceea6d086c8b35adb6ad94d5
Iris <irinaais90@yandex.ru>
2022-09-16 18:21:15 +0300

    feat: 1) adjust text size and background-color in App.css 2) add link in Promo.js

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -9,7 +9,7 @@ function Promo() {
         <div className="promo__info">
           <h1 className="promo__title">Учебный проект студента факультета Веб&#8209;разработки.</h1>
           <p className="promo__description">Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-          <a className="promo__button" href="#" target="_blank">Узнать больше</a>
+          <a className="promo__button" href="https://practicum.yandex.ru/web/" target="_blank">Узнать больше</a>
         </div>
         <img alt="Логотип планеты" src={ planet } className="promo__image"/>
       </div>
