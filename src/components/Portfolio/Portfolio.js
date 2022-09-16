af84ac9b59ac2dc9ddffc4b3e192af489aa7962d
Iris <irinaais90@yandex.ru>
2022-09-16 17:27:29 +0300

    fix: add arrows in Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Portfolio.css";
+import arrow from "../../images/strelka.svg";
 
 function Portfolio() {
   return (
@@ -7,13 +8,22 @@ function Portfolio() {
       <h2 className="portfolio__header">Портфолио</h2>
       <ul className="portfolio__list">
         <li className="portfolio__item">
-          <a className="portfolio__link" href="https://github.com/irinaais/how-to-learn" target="_blank">Статичный сайт</a>
+          <a className="portfolio__link" href="https://github.com/irinaais/how-to-learn" target="_blank">
+            <p className="portfolio__link-text">Статичный сайт</p>
+            <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
+          </a>
         </li>
         <li className="portfolio__item">
-          <a className="portfolio__link" href="https://github.com/irinaais/russian-travel" target="_blank">Адаптивный сайт</a>
+          <a className="portfolio__link" href="https://github.com/irinaais/russian-travel" target="_blank">
+            <p className="portfolio__link-text">Адаптивный сайт</p>
+            <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
+          </a>
         </li>
         <li className="portfolio__item">
-          <a className="portfolio__link" href="http://mesto.irinaosipova.nomoredomains.sbs/" target="_blank">Одностраничное приложение</a>
+          <a className="portfolio__link" href="http://mesto.irinaosipova.nomoredomains.sbs/" target="_blank">
+            <p className="portfolio__link-text">Одностраничное приложение</p>
+            <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
+          </a>
         </li>
       </ul>
     </section>
