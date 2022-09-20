f7d02abed3a148710d5c6326d112e15d17592b71
Iris <irinaais90@yandex.ru>
2022-09-20 12:43:48 +0300

    feat: add media queries for Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,27 +5,29 @@ import arrow from "../../images/strelka.svg";
 function Portfolio() {
   return (
     <section className="portfolio">
-      <h2 className="portfolio__header">Портфолио</h2>
-      <ul className="portfolio__list">
-        <li className="portfolio__item">
-          <a className="portfolio__link" href="https://github.com/irinaais/how-to-learn" target="_blank">
-            <p className="portfolio__link-text">Статичный сайт</p>
-            <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
-          </a>
-        </li>
-        <li className="portfolio__item">
-          <a className="portfolio__link" href="https://github.com/irinaais/russian-travel" target="_blank">
-            <p className="portfolio__link-text">Адаптивный сайт</p>
-            <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
-          </a>
-        </li>
-        <li className="portfolio__item">
-          <a className="portfolio__link" href="http://mesto.irinaosipova.nomoredomains.sbs/" target="_blank">
-            <p className="portfolio__link-text">Одностраничное приложение</p>
-            <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
-          </a>
-        </li>
-      </ul>
+      <div className="portfolio__container">
+        <h2 className="portfolio__header">Портфолио</h2>
+        <ul className="portfolio__list">
+          <li className="portfolio__item">
+            <a className="portfolio__link" href="https://github.com/irinaais/how-to-learn" target="_blank">
+              <p className="portfolio__link-text">Статичный сайт</p>
+              <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
+            </a>
+          </li>
+          <li className="portfolio__item">
+            <a className="portfolio__link" href="https://github.com/irinaais/russian-travel" target="_blank">
+              <p className="portfolio__link-text">Адаптивный сайт</p>
+              <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
+            </a>
+          </li>
+          <li className="portfolio__item">
+            <a className="portfolio__link" href="http://mesto.irinaosipova.nomoredomains.sbs/" target="_blank">
+              <p className="portfolio__link-text">Одностраничное приложение</p>
+              <img className="portfolio__contacts-arrow" alt="Изображение стрелки" src={ arrow }/>
+            </a>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
