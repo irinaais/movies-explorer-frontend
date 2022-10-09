229fb88f0bea1034eedbba513a6d07bb50aee2ed
Iris <irinaais90@yandex.ru>
2022-10-09 17:09:11 +0400

    feat: 1) add MoviesCard MoviesCardList without Header and other permissions

---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./MoviesCard.css";
+
+function MoviesCard(props) {
+  return (
+    <li className="movies-card">
+      <div className="movies-card__description">
+        <h3 className="movies-card__title">{props.title}</h3>
+        <p className="movies-card__duration">{props.duration}</p>
+        <button className={props.button}/>
+      </div>
+      <img className="movies-card__image" alt={props.title} src={props.image}/>
+    </li>
+  );
+}
+
+export default MoviesCard;
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,11 +1,30 @@
 import React from "react";
 import "./MoviesCardList.css";
+import MoviesCard from "../MoviesCard/MoviesCard";
+import design from "../../images/33_slova_o_design.svg";
+import hundredYears from "../../images/1000_let_design.svg";
+import benksi from "../../images/v_pogone_za_benksi.svg";
+import baskiya from "../../images/vzriv_realnosti.svg";
+import run from "../../images/beg_eto_svoboda.svg";
+import booksellers from "../../images/knigotorgovthsi.svg";
+import germany from "../../images/kogda_ya_dumayu_o_germanii_nochyu.svg";
+const cardLikeButtonClassName = "movies-card__button movies-card__button_like";
+const cardDislikeButtonClassName = "movies-card__button movies-card__button_dislike";
 
 function MoviesCardList() {
   return (
-    <>
-
-    </>
+    <section className="movies-card-list">
+      <ul className="movies-card-list__container">
+        <MoviesCard title="33 слова о дизайне" duration="1ч 42м" image={design} button={cardLikeButtonClassName}/>
+        <MoviesCard title="Киноальманах «100 лет дизайна»" duration="1ч 42м" image={hundredYears} button={cardLikeButtonClassName}/>
+        <MoviesCard title="В погоне за Бенкси" duration="1ч 42м" image={benksi} button={cardDislikeButtonClassName}/>
+        <MoviesCard title="Баския: Взрыв реальности" duration="1ч 42м" image={baskiya} button={cardDislikeButtonClassName}/>
+        <MoviesCard title="Бег это свобода" duration="1ч 42м" image={run} button={cardLikeButtonClassName}/>
+        <MoviesCard title="Книготорговцы" duration="1ч 42м" image={booksellers} button={cardDislikeButtonClassName}/>
+        <MoviesCard title="Когда я думаю о Германии ночью" duration="1ч 42м" image={germany} button={cardDislikeButtonClassName}/>
+      </ul>
+      <button className="movies-card-list__button" type="button">Ещё</button>
+    </section>
   );
 }
 
