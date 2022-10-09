ca92cc8502891b8a9a7fae84c022bb50df46a02c
Iris <irinaais90@yandex.ru>
2022-10-10 00:58:35 +0400

    feat: finished MoviesCard and MoviesCardList

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -5,8 +5,10 @@ function MoviesCard(props) {
   return (
     <li className="movies-card">
       <div className="movies-card__description">
-        <h3 className="movies-card__title">{props.title}</h3>
-        <p className="movies-card__duration">{props.duration}</p>
+        <div className="movies-card__info">
+          <h3 className="movies-card__title">{props.title}</h3>
+          <p className="movies-card__duration">{props.duration}</p>
+        </div>
         <button className={props.button}/>
       </div>
       <img className="movies-card__image" alt={props.title} src={props.image}/>
