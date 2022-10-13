54ff8e442072dc9b30053af17299ff5f8ece34e7
Iris <irinaais90@yandex.ru>
2022-10-13 20:59:48 +0400

    feat: add "/saved-movies"

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./SavedMovies.css";
 import Header from "../Header/Header";
+import SearchForm from "../SearchForm/SearchForm";
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
 
 function SavedMovies(props) {
   return (
     <section className="saved-movies">
       <Header theme={"header_theme_dark"} loggedIn={props.loggedIn}/>
+      <SearchForm/>
+      <MoviesCardList/>
     </section>
   );
 }
