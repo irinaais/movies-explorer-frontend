fdeb8e38318874edf36402cb7c7f2948170449fd
Iris <irinaais90@yandex.ru>
2022-10-17 23:29:46 +0400

    feat: add button disabling in Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,25 @@
-import React, {useState} from "react";
+import React, { useRef, useState } from "react";
 import "./Login.css";
 import logo from "../../images/logo.svg";
 import { Link } from "react-router-dom";
 
 function Login() {
+  const formRef = useRef();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  function validationForm() {
+    const formTag = formRef.current;
+    if (formTag.checkValidity()) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }
 
   function handleChangeEmail(evt) {
     setEmail(evt.target.value);
@@ -16,6 +28,7 @@ function Login() {
     } else {
       setEmailError("Введён некорректный e-mail");
     }
+    validationForm();
   }
 
   function handleChangePassword(evt) {
@@ -25,6 +38,7 @@ function Login() {
     } else {
       setPasswordError("Минимум - 8 символов, максимум - 30");
     }
+    validationForm();
   }
 
   return (
@@ -33,7 +47,7 @@ function Login() {
         <Link className="login__button login__button_logo" to="/">
           <img alt="Логотип" src={ logo } className="login__logo"/>
         </Link>
-        <form className="login__form" noValidate>
+        <form ref={ formRef } className="login__form" noValidate>
           <h2 className="login__title">Рады видеть!</h2>
           <fieldset className="login__info">
             <div className="login__input-container">
@@ -68,7 +82,7 @@ function Login() {
               <span className="login__input-error passwordUser-input-error">{passwordError}</span>
             </div>
           </fieldset>
-          <button className="login__button login__button_signin" type="submit" aria-label="Авторизоваться">Войти</button>
+          <button className="login__button login__button_signin" type="submit" aria-label="Авторизоваться" disabled={ isDisabled }>Войти</button>
           <h3 className="login__text-register">Ещё не зарегистрированы?
             <Link className="login__button login__button_text-register" to="/signup">Регистрация</Link>
           </h3>
