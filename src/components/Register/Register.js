3344382d5f389d008b784a50d53e81cc25c89626
Iris <irinaais90@yandex.ru>
2022-10-17 23:17:27 +0400

    feat: add button disabling in Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,15 +1,27 @@
-import React, {useState} from "react";
+import React, { useRef, useState } from "react";
 import "./Register.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../../images/logo.svg";
 
 function Register() {
+  const formRef = useRef();
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [nameError, setNameError] = useState("");
   const [emailError, setEmailError] = useState("");
   const [passwordError, setPasswordError] = useState("");
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  function validationForm() {
+    const formTag = formRef.current;
+    if (formTag.checkValidity()) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }
 
   function handleChangeName(evt) {
     setName(evt.target.value);
@@ -18,6 +30,7 @@ function Register() {
     } else {
       setNameError("Минимум - 2 символа, максимум - 40");
     }
+    validationForm();
   }
 
   function handleChangeEmail(evt) {
@@ -27,6 +40,7 @@ function Register() {
     } else {
       setEmailError("Введён некорректный e-mail");
     }
+    validationForm();
   }
 
   function handleChangePassword(evt) {
@@ -36,15 +50,16 @@ function Register() {
     } else {
       setPasswordError("Минимум - 8 символов, максимум - 30");
     }
+    validationForm();
   }
 
   return (
     <section className="register">
       <div className="register__container">
         <Link className="register__button register__button_logo" to="/">
-          <img alt="Логотип" src={logo} className="register__logo"/>
+          <img alt="Логотип" src={ logo } className="register__logo"/>
         </Link>
-        <form className="register__form">
+        <form ref={ formRef } className="register__form" noValidate>
           <h2 className="register__title">Добро пожаловать!</h2>
           <fieldset className="register__info">
             <div className="register__input-container">
@@ -60,7 +75,7 @@ function Register() {
                 value={name || ""}
                 onChange={handleChangeName}
               />
-              <span className="register__input-error nameUser-input-error">{nameError}</span>
+              <span className="register__input-error nameUser-input-error">{ nameError }</span>
             </div>
             <div className="register__input-container">
               <label className="register__label">E-mail</label>
@@ -75,7 +90,7 @@ function Register() {
                 onChange={handleChangeEmail}
                 pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,63}$"
               />
-              <span className="register__input-error emailUser-input-error">{emailError}</span>
+              <span className="register__input-error emailUser-input-error">{ emailError }</span>
             </div>
             <div className="register__input-container">
               <label className="register__label">Пароль</label>
@@ -90,10 +105,10 @@ function Register() {
                 value={password || ""}
                 onChange={handleChangePassword}
               />
-              <span className="register__input-error passwordUser-input-error">{passwordError}</span>
+              <span className="register__input-error passwordUser-input-error">{ passwordError }</span>
             </div>
           </fieldset>
-          <button className="register__button register__button_signin" type="submit" aria-label="Зарегистрироваться">Зарегистрироваться</button>
+          <button className="register__button register__button_signin" type="submit" aria-label="Зарегистрироваться" disabled={ isDisabled }>Зарегистрироваться</button>
           <h3 className="register__text-register">Уже зарегистрированы?
             <Link className="register__button register__button_text-register" to="/signin">Войти</Link>
           </h3>
