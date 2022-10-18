44cd27795cc4dbdd041e0c7c6f04da8741f9e871
Iris <irinaais90@yandex.ru>
2022-10-18 18:29:51 +0400

    fix: changed the structure of the code in PageNotFound.js

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 function PageNotFound() {
   return (
-    <section className="page-not-found">
+    <main className="page-not-found">
       <div className="page-not-found__container">
         <div className="page-not-found__info">
           <h2 className="page-not-found__title">404</h2>
@@ -12,7 +12,7 @@ function PageNotFound() {
         </div>
         <Link className="page-not-found__link" to={-1}>Назад</Link>
       </div>
-    </section>
+    </main>
   );
 }
 
