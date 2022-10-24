2df685f4e68480c9274a6e6cca4ec2474578b8d0
Iris <irinaais90@yandex.ru>
2022-10-24 11:49:05 +0400

    feat: add like and dislike without api

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./MoviesCard.css";
 
 function MoviesCard(props) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  function handleLikeClick() {
+    if (!isLiked) {
+      setIsLiked(true);
+    } else {
+      setIsLiked(false);
+    }
+  }
+
   return (
     <li className="movies-card">
       <div className="movies-card__description">
@@ -9,7 +19,11 @@ function MoviesCard(props) {
           <h3 className="movies-card__title">{props.title}</h3>
           <p className="movies-card__duration">{props.duration}</p>
         </div>
-        <button className={props.button} aria-label="Кнопка лайка или удаления" type="button"/>
+        <button
+          className={isLiked ? "movies-card__button movies-card__button_like" : "movies-card__button movies-card__button_dislike"}
+          aria-label="Кнопка лайка или удаления"
+          type="button"
+          onClick={handleLikeClick}/>
       </div>
       <img className="movies-card__image" alt={props.title} src={props.image}/>
     </li>
