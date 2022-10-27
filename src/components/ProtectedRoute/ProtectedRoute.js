b16e387524bdb89c32e239dcd5b30300774067d8
Iris <irinaais90@yandex.ru>
2022-10-27 18:27:05 +0400

    fix: link in ProtectedRout.js

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,7 +1,7 @@
 import React, { Navigate, Outlet } from "react-router-dom";
 
 const ProtectedRoute = (props) => {
-  return props.loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
+  return props.loggedIn ? <Outlet /> : <Navigate to="/signin" />;
 }
 
 export default ProtectedRoute;
