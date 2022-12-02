330c86614bbb1e9445ddc928f637eaa8ac9043c9
Iris <irinaais90@yandex.ru>
2022-12-02 21:05:11 +0400

    fix: change about-me__text

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -10,11 +10,11 @@ function AboutMe() {
         <div className="about-me__info-container">
           <div className="about-me__info">
             <h3 className="about-me__name">Ирина</h3>
-            <p className="about-me__description">Фронтенд-разработчик, 32 года</p>
+            <p className="about-me__description">Веб-разработчик, 32 года</p>
             <p className="about-me__text">На данный момент я живу в Грузии. Высшее образование получила в Орловском
               государственном университете по специальности «Документоведение и документационное обеспечение управления»,
               но меня всегда очень привлекало программирование. В ноябре 2022 года закончила обучение в Яндекс.Практикуме
-              на фронтенд-разработчика. Люблю читать, играть в компьютерные игры и хочу связать свою дальнейшую карьеру с программированием.</p>
+              на веб-разработчика. Люблю читать, играть в компьютерные игры и хочу связать свою дальнейшую карьеру с программированием.</p>
             <ul className="about-me__contacts-list">
               <li className="about-me__contacts-item">
                 <a className="about-me__contacts-link" href="https://github.com/irinaais" target="_blank" rel="noreferrer">Github</a>
